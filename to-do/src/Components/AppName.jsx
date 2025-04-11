0f19d1d43069fdc846291fefbc528b84ca33a576
Zunaid <ja7667924@gmail.com>
2025-04-11 19:26:27 +0530

    recreate to-do app

--- to-do/src/Components/AppName.jsx
+++
@@ -1,7 +0,0 @@
-function AppName() {
-    return (
-        <h1>ToDo</h1>
-    );
-}
-
-export default AppName;
--- to-do/src/main.jsx
+++ to-do/src/main.jsx
@@ -1,11 +1,10 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.jsx'
-// import "bootstrap/dist/css/bootstrap.min.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
