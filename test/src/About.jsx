7cadd7d23cf35c82353b86e53f8837b3e7210bcc
Zunaid <ja7667924@gmail.com>
2025-09-08 21:59:22 +0530

    some minor updates

--- test/src/About.jsx
+++ test/src/About.jsx
@@ -1,7 +1,35 @@
+import {useEffect, useState} from "react";
+
 function About() {
-    return (
-        <h1>This is a about page</h1>
-    );
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async function () {
+            setLoading(true);
+            let res = await fetch("https://jsonplaceholder.typicode.com/posts");
+            setData(await res.json());
+            setLoading(false);
+        })();
+    }, []);
+
+    if (loading) {
+        return <h1>Loading...</h1>;
+    }
+
+    return <div>
+        <h1>Data Founded</h1>
+        {data && data.slice(0, 5).map((d, i) => {
+            return <div key={i} style={{
+                marginBottom: '10px',
+            }}>
+                <h1>user id: {d.userId}</h1>
+                <h1>id: {d.id}</h1>
+                <h1>Title: {d.title}</h1>
+                <h1>Body: {d.body}</h1>
+            </div>
+        })}
+    </div>
 }
 
 export default About;
--- test/src/Home.jsx
+++ test/src/Home.jsx
@@ -1,7 +1,19 @@
+import {useState} from "react";
+
 function Home() {
-    return (
-        <h1>Home Page</h1>
-    );
+    const [count, setCount] = useState(0);
+
+    return <div>
+        <h1>Counter</h1>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(c => c + 1)}>Increment</button>
+        <button onClick={() => setCount(c => {
+            if (c === 0) return c;
+            return c - 1;
+        })}>Decrement
+        </button>
+        <button onClick={() => setCount(0)}>Reset</button>
+    </div>;
 }
 
 export default Home;
